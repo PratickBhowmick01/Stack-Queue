d08f297de78d948fd051c1ba717aae45563c3eee
Pratick Bhowmick <pratickbhowmick01@gmail.com>
2026-01-19 23:51:02 +0600

    Add Stack implementation using array and linked list
    Implemented two Stack classes: one using an array and another using a linked list.

---
+++ stack.py
@@ -0,0 +1,47 @@
+#Stack Implementation//- Array
+class Stack:
+    stack = []
+    pointer = -1
+    
+    def push(self, element):
+        self.stack.append(element)
+        self.pointer += 1
+    
+    def peek(self):
+        return (self.stack[self.pointer])
+
+    def pop(self):
+        value = self.stack[self.pointer]
+        self.stack = self.stack[:-1]    #discarding the last item
+        self.pointer -= 1
+        return value  
+
+#Stack Implementation// Linked List
+class Node:
+    def __init__(self,v):
+        self.val = v
+        self.ref = None 
+        
+class Stack:
+    head = None 
+    
+    def push(self, data):
+        if(self.head == None):
+            self.head = Node(data) 
+        else:
+            n = Node(data) 
+            n.ref = self.head
+            self.head = n 
+    
+    def peek(self):
+        #check for empty list
+        return self.head.val 
+    
+    def pop(self):
+        #check for empty list 
+        temp = self.head 
+        self.head = self.head.ref 
+        value = temp.val  
+        temp.val = None 
+        temp.ref = None 
+        return value
